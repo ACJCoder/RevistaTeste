f6481541f2f15c7583cab6f2443d11ad6cbc57c3
Antonio Cabral <studiopronto1@gmail.com>
2017-11-12 19:42:24 -0200

    inicio do projeto

---
+++ RevistaTeste/RevistaTeste/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RevistaTeste.Startup))]
+namespace RevistaTeste
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
